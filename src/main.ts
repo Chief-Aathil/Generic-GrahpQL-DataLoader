1808f9fc8963bf1401fe006a96df63a713807401
Chief-Aathil <aathilr7@gmail.com>
2022-07-14 13:34:55 +0530

    Moved simple paramdecorator to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
 export function myTSFuction() {
     console.log('Hello from myTSFunction');
     
 }
+
+export const InjectDataloaderv2 = createParamDecorator(
+    async (data: any, context: ExecutionContext) => {
+        return `data:${data}\ncontext:${context}`
+    }
+)
--- src/scratch.ts
+++
@@ -1,9 +0,0 @@
-import {
-    createParamDecorator, ExecutionContext
-} from '@nestjs/common'
-
-export const InjectDataloaderv2 = createParamDecorator(
-    async (data: any, context: ExecutionContext) => {
-        return `data:${data}\ncontext:${context}`
-    }
-)
